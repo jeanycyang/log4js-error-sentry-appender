7becec2821b85eacbbc7d82ed9cd81a18d02415e
Jean Yang <bdlas321@gmail.com>
2018-12-27 18:19:08 +0800

    feat(sentry-fingerprint): add "useCategoryAsFingerprint" option (grouping events by log4js category)
    ref: https://docs.sentry.io/data-management/rollups/?platform=node#custom-grouping

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const { levels } = require('log4js');
 const Sentry = require('@sentry/node');
 
-function errorSentryAppender() {
+function errorSentryAppender(layout, { useCategoryAsFingerprint = false }) {
   return (loggingEvent) => {
-    const { level, data, context } = loggingEvent;
+    const { level, data, context, categoryName } = loggingEvent;
     if (level.level >= levels.ERROR.level) {
       const levelStr = level.levelStr.toLowerCase();
       const [error] = data;
       Sentry.configureScope((scope) => {
         if (context.user) scope.setUser(context.user);
+        if (useCategoryAsFingerprint) {
+          if (error.name) {
+            scope.setFingerprint([categoryName, error.name]);
+          } else {
+            scope.setFingerprint([categoryName]);
+          }
+        }
+        scope.setTag('category', categoryName);
         scope.setLevel(levelStr);
       });
       Sentry.captureException(error);
@@ -20,7 +28,7 @@ function configure(config, layouts) {
   let layout = layouts.colouredLayout;
 
   // sentry config reference: https://docs.sentry.io/error-reporting/configuration/?platform=node
-  const { sentryConfig } = config;
+  const { sentryConfig, useCategoryAsFingerprint } = config;
 
   if (!sentryConfig.dsn) {
     throw new Error('Must provide Sentry DSN!');
@@ -32,7 +40,7 @@ function configure(config, layouts) {
     layout = layouts.layout(config.layout.type, config.layout);
   }
 
-  return errorSentryAppender(layout);
+  return errorSentryAppender(layout, { useCategoryAsFingerprint });
 }
 
 exports.errorSentryAppender = errorSentryAppender;
